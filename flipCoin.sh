d4f65c517c97edba834a163175716f8e7be6f304
sagar-528 <gupta.sagar528@gmail.com>
2020-03-21 01:57:32 +0530

    ADDED: DictionaryAndPercentage

--- flipCoin.sh
+++ flipCoin.sh
@@ -7,9 +7,30 @@
 HEADS=0
 TAILS=1
 
-if ((RANDOM%2 == 0))
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+declare -A Coin
+
+Coin=([H]=0 [T]=0)		#Declare Dictionary.
+
+#Times of Fliping for singlet combination
+read -p "How many times you want to flip the coin = " noTimeFlip
+
+for ((flipcount=1; flipcount<=noTimeFlip; flipcount++))
+do
+	case $((RANDOM%2)) in
+		0)
+		  Coin[H]=$((${Coin[H]}+1)) ;;
+		1)
+		  Coin[T]=$((${Coin[T]}+1)) ;;
+	esac
+done
+
+#Printing the singlet combination
+for key in "${!Coin[@]}"
+do
+	echo "${key} = ${Coin[$key]}"
+done
+
+#Percentage of singlet combination
+Percentage=`echo " scale=4 ; (${Coin[H]}*100)/$noTimeFlip" | bc -l`
+echo "The percentage of head occurance is :" $Percentage
+
